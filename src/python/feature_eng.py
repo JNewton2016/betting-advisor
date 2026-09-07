865b46795b9767ffe29e82babf50728b107acd67
Jacob <jn265@student.le.ac.uk>
2026-09-07 16:52:06 +0100

    create skeleton for frontend and ML process

---
+++ src/python/feature_eng.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+#function to engineer data columns into features for training
+def engineer_features (df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    
+
+
+#columns used as features in training
+feature_cols - [
+    """
+
+    """
+]
+
